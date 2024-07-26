310a5b46e87689174860789530348248db7a1886
jigong2024 <choijming21@naver.com>
2024-07-26 11:42:41 +0900

    2번째 실습도 잘마무리

---
+++ 240724/solution1.js
@@ -0,0 +1,16 @@
+const greeting = "Hello";
+const name = "World";
+// const no = " ";
+
+// const add = greeting.concat(no, name);
+// console.log(`${greeting} ${name}`);
+
+// console.log(add) // "Hello World" 구현
+
+
+const a = "안녕";
+const b = "반가워";
+
+console.log(`${a} 현수야, ${b}.`);
+
+// console.log(greeting + " " + name);
---
+++ 240724/solution2.js
@@ -0,0 +1,8 @@
+const lion = { 
+    이름: "삼바",
+    나이: 3,
+    주인공: true,
+    대사: "하쿠나마타타",
+ }
+
+ console.log(lion.대사);
